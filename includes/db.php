7a4ae15ffc9694be0339e2f13eac96a1c48342ac
Greta Sabaliauskienė <sabaliauskiene@yahoo.com>
2020-08-16 17:13:00 +0300

    test

---
+++ includes/db.php
@@ -0,0 +1,9 @@
+<?php 
+
+$conn = mysqli_connect('localhost', 'root', 'mysql', 'cms');
+if ($conn) {
+   echo "Connection succesfull!!";
+} else {
+ die("Database connection failed");
+}
+?>
---
+++ includes/footer.php
@@ -0,0 +1,16 @@
+
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; Your Website 2014</p>
+                </div>
+            </div>
+        </footer>
+    <script src="js/jquery.js"></script>
+
+
+<script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
